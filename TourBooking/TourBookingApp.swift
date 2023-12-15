7ffe9e65975d9cd64770614581eac7b2688b0d59
DmitryGorbunow <dmitry.gorbunow@gmail.com>
2023-12-15 19:20:55 +0500

    Initial Commit

---
+++ TourBooking/TourBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourBookingApp.swift
+//  TourBooking
+//
+//  Created by Dmitry Gorbunow on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TourBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
